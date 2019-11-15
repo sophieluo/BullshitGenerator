bddd9f5d62aeec816b998cadde9b5cf2bff5bec7
sophieluo <sophie@groobusiness.com>
2019-11-15 15:16:18 -0500

    add basic logic of bullshit generator to articleString then parse in skillBuilder

--- fact.js
+++ fact.js
@@ -150,6 +150,90 @@ const LocalizationInterceptor = {
   }
 };
 
+//start bullshit generator
+let topic = "what's the weather today"
+
+let argument = [
+    "In theory, ",
+    "In summary, ",
+    "Therefore, ",
+    "However, ",
+]
+
+let quotes = [
+    "Lincoln said, 123",
+    "Someone said, 135",
+    "Another person said, 365",
+]
+
+let after = [
+    "I hope you can understand this. ",
+    "This is a short but powerful argument. ",
+]
+
+let before = [
+    "once said, "
+]
+
+function randomSent(list){
+    let i = Math.floor( Math.random() * list.length );
+    return list[i];
+}
+
+function randomNum(min = 0, max = 100){
+    let num = Math.random()*( max - min ) + min;
+    return num;
+}
+
+function createQuotes(){
+    let quote = (quotes)
+    return quote
+}
+
+function createArguments(){
+    let sentence = randomSent(argument);
+    sentence = sentence.replace(RegExp("topic", "g"),topic);
+    return sentence;
+}
+
+function createParagragh(paragraph){
+    if(paragraph[paragraph.length-1] === " "){
+        paragraph = paragraph.slice(0,-2)
+    }
+    return "　　" + paragraph + "。 "
+}
+
+function createArticle(){
+    let article = []
+    for(let empty in topic){
+        let paragraph = "";
+        let paragraphLength = 0;
+        while( paragraphLength < 3000 ){
+            let random = randomNum();
+            if(random < 5 && paragraph.length > 200){
+                paragraph = createParagragh(paragraph);
+                article.push(paragraph);
+                paragraph = "";
+            }else if(random < 20){
+                let sentence = createQuotes();
+                paragraphLength = paragraphLength + sentence.length;
+                paragraph = paragraph + sentence;
+            }else{
+                let sentence = createArguments();
+                paragraphLength = paragraphLength + sentence.length;
+                paragraph = paragraph + sentence;
+            }
+        }
+        paragraph = createParagragh(paragraph);
+        article.push(paragraph);
+    }
+    return article.join("\n");
+}
+
+const articleString = JSON.stringify(createArticle());
+console.log(articleString);
+//end bullshit generator
+
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
@@ -180,14 +264,7 @@ const enData = {
     FALLBACK_REPROMPT: 'What can I help you with?',
     ERROR_MESSAGE: 'Sorry, an error occurred.',
     STOP_MESSAGE: 'Goodbye!',
-    FACTS:
-      [
-        'A year on Mercury is just 88 days long.',
-        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-        'On Mars, the Sun appears about half the size as it does on Earth.',
-        'Jupiter has the shortest day of all the planets.',
-        'The Sun is an almost perfect sphere.',
-      ],
+    FACTS: articleString,
   },
 };
 
--- index.js
+++ index.js
@@ -1,135 +1,277 @@
-exports.handler = async (event) => {
+/*
+ * Copyright 2018-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
-    //launch request
-    const LaunchHandler = {
-        canHandle(handlerInput) {
-            const request = handlerInput.requestEnvelope.request;
+//
+// Alexa Fact Skill - Sample for Beginners
+//
 
-            return request.type === 'LaunchRequest';
-        },
-        handle(handlerInput) {
-            const attributesManager = handlerInput.attributesManager;
-            const responseBuilder = handlerInput.responseBuilder;
+// sets up dependencies
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
 
-            const requestAttributes = attributesManager.getRequestAttributes();
-            const speechOutput = `${requestAttributes.t('WELCOME')} ${requestAttributes.t('HELP')}`;
-            return responseBuilder
-                .speak(speechOutput)
-                .reprompt(speechOutput)
-                .getResponse();
+// core functionality for fact skill
+const GetNewFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    // checks request type
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name === 'GetNewFactIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    // gets a random fact by assigning an array to the variable
+    // the random item from the array will be selected by the i18next library
+    // the i18next library is set up in the Request Interceptor
+    const randomFact = requestAttributes.t('FACTS');
+    // concatenates a standard message with the random fact
+    const speakOutput = requestAttributes.t('GET_FACT_MESSAGE') + randomFact;
 
-        },
-    };
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      // Uncomment the next line if you want to keep the session open so you can
+      // ask for another fact without first re-opening the skill
+      // .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .withSimpleCard(requestAttributes.t('SKILL_NAME'), randomFact)
+      .getResponse();
+  },
+};
 
-    let topic = "what's the weather today"
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('HELP_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .getResponse();
+  },
+};
 
-    let argument = [
-        "In theory, ",
-        "In summary, ",
-        "Therefore, ",
-        "However, ",
-    ]
+const FallbackHandler = {
+  // The FallbackIntent can only be sent in those locales which support it,
+  // so this handler will always be skipped in locales where it is not supported.
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.FallbackIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('FALLBACK_MESSAGE'))
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT'))
+      .getResponse();
+  },
+};
 
-    let quotes = [
-        "Lincoln said, 123",
-        "Someone said, 135",
-        "Another person said, 365",
-    ]
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('STOP_MESSAGE'))
+      .getResponse();
+  },
+};
 
-    let after = [
-        "I hope you can understand this. ",
-        "This is a short but powerful argument. ",
-    ]
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
 
-    let before = [
-        "once said, "
-    ]
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('ERROR_MESSAGE'))
+      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
+      .getResponse();
+  },
+};
 
-    function randomSent(list){
-        let i = Math.floor( Math.random() * list.length );
-        return list[i];
-    }
-
-    function randomNum(min = 0, max = 100){
-        let num = Math.random()*( max - min ) + min;
-        return num;
-    }
-
-    function createQuotes(){
-        let quote = (quotes)
-        return quote
-    }
-
-    function createArguments(){
-        let sentence = randomSent(argument);
-        sentence = sentence.replace(RegExp("topic", "g"),topic);
-        return sentence;
-    }
-
-    function createParagragh(paragraph){
-        if(paragraph[paragraph.length-1] === " "){
-            paragraph = paragraph.slice(0,-2)
-        }
-        return "　　" + paragraph + "。 "
-    }
-
-    function createArticle(){
-        let article = []
-        for(let empty in topic){
-            let paragraph = "";
-            let paragraphLength = 0;
-            while( paragraphLength < 3000 ){
-                let random = randomNum();
-                if(random < 5 && paragraph.length > 200){
-                    paragraph = createParagragh(paragraph);
-                    article.push(paragraph);
-                    paragraph = "";
-                }else if(random < 20){
-                    let sentence = createQuotes();
-                    paragraphLength = paragraphLength + sentence.length;
-                    paragraph = paragraph + sentence;
-                }else{
-                    let sentence = createArguments();
-                    paragraphLength = paragraphLength + sentence.length;
-                    paragraph = paragraph + sentence;
-                }
-            }
-            paragraph = createParagragh(paragraph);
-            article.push(paragraph);
-        }
-        return article.join("\n");
-    }
-
-    const articleString = JSON.stringify(createArticle())
-
-    const response = {
-      "version": "1.0",
-      "sessionAttributes": {
-        "supportedHoroscopePeriods": {
-          "daily": true,
-          "weekly": false,
-          "monthly": false
-        }
-      },
-      "response": {
-        "outputSpeech": {
-          "type": "PlainText",
-          "text": articleString
-        },
-        "card": {
-          "type": "Simple",
-          "title": "Horoscope",
-          "content": articleString
-        },
-        "reprompt": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Can I help you with anything else?"
-          }
-        },
-        "shouldEndSession": false
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    // Gets the locale from the request and initializes i18next.
+    const localizationClient = i18n.init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      resources: languageStrings,
+      returnObjects: true
+    });
+    // Creates a localize function to support arguments.
+    localizationClient.localize = function localize() {
+      // gets arguments through and passes them to
+      // i18next using sprintf to replace string placeholders
+      // with arguments.
+      const args = arguments;
+      const value = i18n.t(...args);
+      // If an array is used then a random value is selected
+      if (Array.isArray(value)) {
+        return value[Math.floor(Math.random() * value.length)];
       }
+      return value;
+    };
+    // this gets the request attributes and save the localize function inside
+    // it to be used in a handler by calling requestAttributes.t(STRING_ID, [args...])
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function translate(...args) {
+      return localizationClient.localize(...args);
     }
+  }
+};
 
-    // return response;
+//start bullshit generator
+let topic = "what's the weather today"
+
+let argument = [
+    "In theory, ",
+    "In summary, ",
+    "Therefore, ",
+    "However, ",
+]
+
+let quotes = [
+    "Lincoln said, 123",
+    "Someone said, 135",
+    "Another person said, 365",
+]
+
+let after = [
+    "I hope you can understand this. ",
+    "This is a short but powerful argument. ",
+]
+
+let before = [
+    "once said, "
+]
+
+function randomSent(list){
+    let i = Math.floor( Math.random() * list.length );
+    return list[i];
+}
+
+function randomNum(min = 0, max = 100){
+    let num = Math.random()*( max - min ) + min;
+    return num;
+}
+
+function createQuotes(){
+    let quote = (quotes)
+    return quote
+}
+
+function createArguments(){
+    let sentence = randomSent(argument);
+    sentence = sentence.replace(RegExp("topic", "g"),topic);
+    return sentence;
+}
+
+function createParagragh(paragraph){
+    if(paragraph[paragraph.length-1] === " "){
+        paragraph = paragraph.slice(0,-2)
+    }
+    return "　　" + paragraph + "。 "
+}
+
+function createArticle(){
+    let article = []
+    for(let empty in topic){
+        let paragraph = "";
+        let paragraphLength = 0;
+        while( paragraphLength < 100 ){
+            let random = randomNum();
+            if(random < 5 && paragraph.length > 50){
+                paragraph = createParagragh(paragraph);
+                article.push(paragraph);
+                paragraph = "";
+            }else if(random < 20){
+                let sentence = createQuotes();
+                paragraphLength = paragraphLength + sentence.length;
+                paragraph = paragraph + sentence;
+            }else{
+                let sentence = createArguments();
+                paragraphLength = paragraphLength + sentence.length;
+                paragraph = paragraph + sentence;
+            }
+        }
+        paragraph = createParagragh(paragraph);
+        article.push(paragraph);
+    }
+    return article.join("\n");
+}
+
+const articleString = JSON.stringify(createArticle());
+console.log(articleString);
+//end bullshit generator
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetNewFactHandler,
+    HelpHandler,
+    ExitHandler,
+    FallbackHandler,
+    SessionEndedRequestHandler,
+  )
+  .addRequestInterceptors(LocalizationInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .withCustomUserAgent('sample/basic-fact/v2')
+  .lambda();
+
+// TODO: Replace this data with your own.
+// It is organized by language/locale.  You can safely ignore the locales you aren't using.
+// Update the name and messages to align with the theme of your skill
+
+
+const enData = {
+  translation: {
+    SKILL_NAME: 'Academic Articles Generator',
+    GET_FACT_MESSAGE: 'Here\'s your bullshit: ',
+    HELP_MESSAGE: 'You can say tell me a bullshit, or, you can say exit... What can I help you with?',
+    HELP_REPROMPT: 'What can I help you with?',
+    FALLBACK_MESSAGE: 'The Academic Articles Generator skill can\'t help you with that. What can I help you with?',
+    FALLBACK_REPROMPT: 'What can I help you with?',
+    ERROR_MESSAGE: 'Sorry, an error occurred.',
+    STOP_MESSAGE: 'Goodbye!',
+    FACTS: articleString,
+  },
+};
+
+
+// constructs i18n and l10n data structure
+const languageStrings = {
+
+  'en': enData,
+
 };
